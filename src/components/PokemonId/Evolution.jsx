c24e6647aa3ad1c66b2ea3e1b74b1e8a6b61d073
Cristian Vazquez <vazquez_roo@outlook.com>
2023-05-05 15:40:08 -0700

    update4.0

--- src/components/PokemonId/Evolution.jsx
+++ src/components/PokemonId/Evolution.jsx
@@ -9,6 +9,7 @@ function Evolution({evolution, next, previous}) {
   const [evolutionPoke, setEvolutionPoke] = useState([])
   const [evolutionDos, setEvolutionDos] = useState([])
   const [evolutionTres, setEvolutionTres] = useState([])
+  const [evolutionI, setEvolutionI] = useState(false)
 
   console.log(evolution)
 
@@ -21,19 +22,35 @@ function Evolution({evolution, next, previous}) {
         .then(res=> {
 
           const evolutionData = res.data.chain
-          console.log(evolutionData)
+          console.log(evolutionData.envolves_to)
 
-          const evolutionDataDos = res.data.chain.evolves_to[0]
-          console.log(evolutionDataDos)
+          const evolutionDataDos = res.data.chain.evolves_to[0].species
+          console.log(evolutionDataDos.name)
 
           const evolutionDataTres = res.data.chain.evolves_to[0].evolves_to
           console.log(evolutionDataTres)
 
+       
+
           
+          if(!evolutionData.evolves_to ){
+            setEvolutionI(false)
+          }
+          else {
+
           setEvolutionPoke(evolutionData['species'])
+          setEvolutionI(true)
+
+          }
           console.log(evolutionPoke)
 
-          setEvolutionDos(evolutionDataDos['species'])
+          
+
+          
+
+            setEvolutionDos(evolutionDataDos)
+            
+
           console.log(evolutionDos)
 
           if(!evolutionDataTres.length){
@@ -62,44 +79,49 @@ function Evolution({evolution, next, previous}) {
    
    
   return (
-    <section className='flex  w-[100%] '>
+    <>
+    {
+      evolutionI &&  <section className='flex  w-[100%] '>
 
-    <div className='grid justify-center items-center w-[75%] justify-items-center  text-[10px] '>
-      
-    
-        <ImgPokeEvolution url={url} next={next} previous={previous} evolution={evolution}  className={``}/>     
-       <p className='text-center min-[600px]:text-[20px] self-end'>{evolutionPoke?.name}</p>
-
-    </div>
-
-
-    <div className={`flex items-center justify-center text-[10px] ${evolutionDos.name? 'opacity-100': 'opacity-0 absolute invisible'}`}>
-
-       <p className=' text-[15px] min-[600px]:text-[20px] pl-2 text-black'>{'>'}</p>
-
-
-      <div className='grid  justify-center items-center justify-items-center'>
-       <ImgPokeEvolution url={urlDos} next={next} previous={previous}/>
-       <p className=' text-[15px] text-center min-[600px]:text-[20px] self-end '>{evolutionDos?.name}</p>
-
-       </div>
-
-    </div>
-
-    <div className={`flex items-center   justify-center text-[10px] ${evolutionTres.name? 'opacity-100': 'opacity-0 absolute invisible'}`}>
-
-    <p className='text-[20px] pl-2  text-black'>{'>'}</p>
-
-      <div className='grid  justify-center items-center'>
+      <div className='grid justify-center items-center w-[75%] justify-items-center  text-[10px] '>
         
-       <ImgPokeEvolution url={urlTres} next={next} previous={previous} className='items-center justify-center self-center justify-self-center' />
-       <p className=' text-[15px] text-center min-[600px]:text-[20px] self-end '>{evolutionTres?.name}</p>
-
-       </div>
-
-    </div>
-
-    </section>
+      
+          <ImgPokeEvolution url={url} next={next} previous={previous} evolution={evolution}  className={``}/>     
+         <p className='text-center text-[15px] min-[600px]:text-[20px] self-end'>{evolutionPoke?.name}</p>
+  
+      </div>
+  
+  
+      <div className={`flex items-center justify-center text-[10px] ${evolutionDos.name? 'opacity-100': 'opacity-0 absolute invisible'}`}>
+  
+         <p className=' text-[15px] min-[600px]:text-[20px] pl-2 text-black'>{'>'}</p>
+  
+  
+        <div className='grid  justify-center items-center justify-items-center'>
+         <ImgPokeEvolution url={urlDos} next={next} previous={previous}/>
+         <p className=' text-[15px] text-center min-[600px]:text-[20px] self-end '>{evolutionDos?.name}</p>
+  
+         </div>
+  
+      </div>
+  
+      <div className={`flex items-center   justify-center text-[10px] ${evolutionTres.name? 'opacity-100': 'opacity-0 absolute invisible'}`}>
+  
+      <p className='text-[20px] pl-2  text-black'>{'>'}</p>
+  
+        <div className='grid  justify-center items-center'>
+          
+         <ImgPokeEvolution url={urlTres} next={next} previous={previous} className='items-center justify-center self-center justify-self-center' />
+         <p className=' text-[15px] text-center min-[600px]:text-[20px] self-end '>{evolutionTres?.name}</p>
+  
+         </div>
+  
+      </div>
+  
+      </section>
+    }
+   
+    </>
 
   )
 }
