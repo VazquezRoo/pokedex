bc78b2252680afd6a41836bc59addbc0cacb4b74
Cristian Vazquez <vazquez_roo@outlook.com>
2023-05-12 18:47:53 -0700

    update

--- src/components/pokedex/FooterPokedex.jsx
+++ src/components/pokedex/FooterPokedex.jsx
@@ -1,37 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function FooterPokedex() {
 
   return (
 
-    <div className='h-[290px] min-w-screen bg-black mt-[40px] grid   justify-items-center justify-center items-end pb-8'>
+  <div className={` h-[300px] bg-black text-white grid justify-center text-[12px] min-[600px]:text-[20px] mt-[20px]`}>
+      <div className='  max-w-[1200px] grid grid-cols-2 p-2'>
 
-          <div className='w-[40%] grid  text-yellow-200 '>
+        <div className='flex flex-col justify-center items-start  '>
+          <h4>Vazquez Roo</h4>
+          <p>Thanks for reading!</p>
+          <p><i className='bx bx-envelope '></i><span className='text-[10px] min-[400px]:text-[12px]  min-[600px]:text-[20px]'> vazquez.roo93@gmail.com</span> </p>
+          <p><i className='bx bx-phone' ></i> (653) 100-7177</p>
+          
+          <p className='pt-[30px] '>&copy; 2023 Cristian Vazquez. All rights reserved.</p>
+         </div>
 
-            <img src="/images/construccion.png" alt="" />
-
-            <div className='grid justify-center'>
-              <h3 className='text-[20px]'>ZONE UNDER</h3>
-              <p className='text-[30px]'>CONSTRUCTION</p>
-            </div>
-
-          </div>
-
-          <div className=' min-[550px]:flex min-[550px]:gap-10 min-[750px]:justify-center min-[750px]:gap-[100px]  min-[550px]:w-[80%] min-[550px]:align-start min-[550px]:justify-items-end '>
-
-                <div className='text-white flex gap-2 items-center '>
-                  <i className='bx bx-envelope'></i>
-                  <p>vazquez.roo93@gmail.com</p>
-                </div>
-
-                <div className='text-white flex gap-2 items-center'>
-                  <i className='bx bx-phone' ></i>
-                  <p>(653) 100-7177</p>
-                </div>
-          </div>
-      
-   </div>
-     
+         <div className='grid justify-center items-center'>
+          <ul className='grid gap-2'>
+            <Link to={'/'} className='hover:text-purple-500'>Home</Link>
+            <Link to={'/pokedex'} className='hover:text-purple-500'>Pokedex</Link>
+            {/* <Link to={'/projects'} className='hover:text-purple-500'>Porjects</Link>
+            <Link to={'/contact'} className='hover:text-purple-500'>Contact</Link> */}
+          </ul>
+         </div>
+         </div>
+    </div>
 
   )
 }
